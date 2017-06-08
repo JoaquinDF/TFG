aeb568e5ff2b7acace9ed53e735abf344a602ce9
Javier Pérez Marcos <jpmarcos@usal.es>
2017-06-08 12:41:01 +0200

    Fix unused task

--- src/innhome/celery.py
+++ src/innhome/celery.py
@@ -9,8 +9,3 @@ app = Celery('innhome')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.timezone = 'Europe/Madrid'
 app.autodiscover_tasks()
-
-
-@app.task(bind=True)
-def debug_task(self):
-    print('Request:{0!r}'.format(self.request))
