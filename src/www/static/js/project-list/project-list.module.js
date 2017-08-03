2ccfec8a5c8ece3f1e094a75fdad8008b6a1fbae
Joaquindf <joaquindf@usal.es>
2017-08-03 14:11:59 +0200

    add tabs bar at index start search code

--- src/www/static/js/project-list/project-list.module.js
+++ src/www/static/js/project-list/project-list.module.js
@@ -1,5 +1,19 @@
 'use strict';
 
 // Define the `projectList` module
-angular.module('projectList', []);
+var plist = angular.module('projectList', []);
 
+plist.filter('myFilter', function () {
+    return function (items, search) {
+        var result = [];
+        angular.forEach(items, function (value, key) {
+            angular.forEach(value, function (value2, key2) {
+                if (value2 === search) {
+                    result.push(value2);
+                }
+            })
+        });
+        return result;
+
+    }
+});
