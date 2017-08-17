fc10a190a4045f22216fe1281dcdb532c159f6a6
Joaquindf <joaquindf@usal.es>
2017-08-17 12:47:27 +0200

    Change Style

---
+++ src/www/static/js/index-scripts.js
@@ -0,0 +1,36 @@
+/**
+ * Created by bisitemini on 17/8/17.
+ */
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
+// Change style of top container on scroll
+
+function openTab(evt, tabName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("tab");
+      debugger;
+
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  debugger;
+  for (i = 0; i < x.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" w3-theme-d1", "");
+
+  }
+  document.getElementById(tabName).style.display = "block";
+    debugger;
+
+  evt.currentTarget.className += " w3-theme-d1";
+    debugger;
+
+
+}
