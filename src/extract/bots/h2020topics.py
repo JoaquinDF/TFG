8673404905cd3ebda2c15e85fc70729c219946a2
Javier Pérez Marcos <jpmarcos@usal.es>
2017-11-15 10:34:41 +0100

    Fix calls url

--- src/extract/bots/h2020topics.py
+++ src/extract/bots/h2020topics.py
@@ -10,7 +10,7 @@ class BotInstance(Bot):
     key = 'topicId'
 
     def process_item(self, db=None):
-        url = 'http://ec.europa.eu/research/participants/portal/data/call/h2020/h2020_topics.json'
+        url = 'http://ec.europa.eu/research/participants/portal/data/call/h2020/topics.json'
         response = requests.get(url=url)
         response.encoding = 'ISO-8859-1'
         docs = json.loads(response.text)['topicData']['Topics']
