3be57cedefdf48f26334df0a0be59cbce8d9f922
Javier Pérez Marcos <jpmarcos@usal.es>
2017-11-22 16:47:52 +0100

    Fix opspatents bot

--- src/extract/bots/opspatents.py
+++ src/extract/bots/opspatents.py
@@ -1,13 +1,50 @@
+import numpy as np
 import epo_ops
 import xmltodict
-import json
+import logging
 
 from utils.bot import Bot
 
 
 class BotInstance(Bot):
     collection = 'bots.epo.patents'
-    key = ''
+    key = '@doc-number'
 
     def process_item(self, db=None):
-        pass
+        collections = [
+            ('data.organizations', 'nombre')
+        ]
+
+        organizations = set()
+        for k, v in collections:
+            organizations = organizations.union(db[k].distinct(v))
+
+        __key__ = 'OQZtQ6amCYzhovfqvXxI9DgOEDPymHzT'
+        __secret__ = 'MJuF1bivcTGMiK1m'
+
+        client = epo_ops.Client(
+            key=__key__,
+            secret=__secret__
+        )
+
+        for organization in sorted(organizations):
+            end = 1
+            total = np.Inf
+            while end <= total:
+                try:
+                    response = client.published_data_search(
+                        cql='ia all "{}"'.format(organization),
+                        range_begin=end,
+                        range_end=end + 24,
+                        constituents=[
+                            'biblio'
+                        ]
+                    )
+                    data = xmltodict.parse(response.text)['ops:world-patent-data']['ops:biblio-search']
+                    for x in data['ops:search-result']['exchange-documents']:
+                        yield x['exchange-document']
+                    total = int(data['@total-result-count'])
+                    end = int(data['ops:range']['@end']) + 1
+                except Exception as e:
+                    logging.debug(e)
+                    break
