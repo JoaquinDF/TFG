af6d055892f558ba2f7d6b4059c3d34db9f8f983
Javier Pérez Marcos <jpmarcos@usal.es>
2017-11-27 13:06:17 +0100

    Fix chromedriver install script

--- bin/chromedriver/install.sh
+++ bin/chromedriver/install.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
-CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb";
-CHROME_DRIVER="https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip";
+CHROME="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+CHROME_DRIVER="https://chromedriver.storage.googleapis.com/2.29/chromedriver_linux64.zip"
 
-set -e;
+set -e
 
-sudo apt-get install libxss1 libappindicator1 libindicator7;
-wget ${CHROME};
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget ${CHROME}
 
-sudo dpkg -i google-chrome*.deb;
-sudo apt-get -f install;
+sudo dpkg -i google-chrome*.deb
+sudo apt-get install -f
 
-sudo apt-get install xvfb;
+sudo apt-get install xvfb
 
-sudo apt-get install unzip;
+sudo apt-get install unzip
 
-wget -N ${CHROME_DRIVER};
-unzip chromedriver_linux64.zip;
-chmod +x chromedriver;
+wget -N ${CHROME_DRIVER}
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
 
-sudo mv -f chromedriver /usr/local/share/chromedriver;
-sudo ln -s -T -f /usr/local/share/chromedriver /usr/local/bin/chromedriver;
-sudo ln -s -T -f /usr/local/share/chromedriver /usr/bin/chromedriver;
+sudo mv -f chromedriver /usr/local/share/chromedriver
+sudo ln -s -T -f /usr/local/share/chromedriver /usr/local/bin/chromedriver
+sudo ln -s -T -f /usr/local/share/chromedriver /usr/bin/chromedriver
 
 sudo rm -r google-chrome*.deb
 sudo rm -r chromedriver_linux64.zip
